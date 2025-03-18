e7a4aa553275de232dfc81d9b84e7ae5ec1b237e
Humu <ahumuzaasiimwe28@gmail.com>
2025-03-18 09:25:01 +0300

    revised loops

---
+++ nested_loops.py
@@ -0,0 +1,10 @@
+for table1 in range (1,6):
+    for table2 in range(1,6):
+        print(f"{table1} X {table2} = {table1*table2}")
+with open('textie.txt' ,'a') as update :
+    update = update.write("\n Day 2 - nested loops. \n I need to pratice more (Useful moderators)")
+for i in range(7):
+    for y in range(7):
+        print("0", end="")
+    print()
+    
--- revision.py
+++ revision.py
@@ -5,13 +5,13 @@ with open('textie.txt','a') as text_file :
     file_name = text_file.write("\n I am going to do this everyday just to get in in mind \n Day 1 -17/03/25 @ 07:23 \n I NEED TO LEARN MORE ADVANCED LOOPS IN MY FREE TIME ")
 
 # dictionaries
-items = {'Bread':5000,'Soda':4500, 'Flour':6000, 'Oil':4900}
+items = {'a000001':5000,'a000003':4500, 'Flour':6000, 'Oil':4900}
 currency = "UGX"
 items['water'] = 2000
 items['Oil'] = 6000
-
+print(items['a000003'])
 # Control flow
-Pin = 1234
+Pin = 12345
 if Pin == 1234 :
     print ("Transaction successful!")
 else :
@@ -32,9 +32,18 @@ table = [1,2,3,4,5,6,7,8,9,10]
 # for num in  table:
     # print("2 x ",num,"=", num *2)
 # While loops
-x = 0
-while x < 100 :
-    x += 1
-    print (x,"x 2 =",x*2 )
-else:
-    print("X is now > 0")
+# x = 0
+# while x < 100 :
+#     x += 1
+#     print (x,"x 2 =",x*2 )
+# else:
+#     print("X is now > 0")
+
+#  Using range. 
+# for i in range(1, 10, 2):  # Generates 1, 3, 5, 7, 9 by adding 2 which is at the end
+#     print(i)
+    
+# Nested loops
+for x in range (4):
+    for y in (0,1,2):
+        print(x,y)
