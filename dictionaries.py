9d512ba571a1191227e666713ed830b3451452c3
Humu <ahumuzaasiimwe28@gmail.com>
2025-03-21 10:00:00 +0300

    Useful operators

--- dictionaries.py
+++ dictionaries.py
@@ -1,15 +1,16 @@
 #Stores values to key(work like variables in a list)
-prices = {'Bread':5000,'soda':4500, 'Choclate':8000}
+prices = {'Bread':5000,'soda':4500, 'Choclate':8000, 'book':9000}
 currency= "UGX"
 total= prices['Bread'] + prices['Choclate'] + prices['soda']
-print(prices['Bread'],currency)
-print(total,currency)
+
+for index, (items, price) in enumerate(prices.items(), start=1):
+    print(f"{index} {items} {price}")
 
 #This is how to add new items
 prices['Water'] = 2000
-print(prices)
+# print(prices)
 
 #This is how to overwrite another item
 prices['Bread'] = 4500 
-print(prices)
-print(prices['Bread'],currency)
+# print(prices)
+# print(prices['Bread'],currency)
---
+++ update.py
@@ -0,0 +1,4 @@
+with open("Daily_update.txt","a") as store:
+    database = store.write(" \n ")
+# "Day 1 -17/03/25 @ 07:23 
+
