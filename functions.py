ddf736ceda25cf51abb75a0baa4283700b2ea7ba
Humu <ahumuzaasiimwe28@gmail.com>
2025-04-26 12:15:50 +0300

    functions.py

--- functions.py
+++ functions.py
@@ -30,6 +30,7 @@ outie = checker(int(4))
 print(outie)
 # Using list
 even_nos =[]
+
 def list_checker(numbers):
     for item in numbers:
         if item %2 == 0:
@@ -37,3 +38,37 @@ def list_checker(numbers):
     return even_nos
 reslt = list_checker([1,2,3,4,5,6,7,8,9,10])
 print(reslt)
+#ADVANCED FUNCTION FOR TEMP CONVERTING AND RETURNIN REQUESTED....
+temp_in_celcius =[]
+cold_temp = []
+def temp_converter(temps):
+    for temp in temps:
+        temp_in_celcius.append((temp - 32)*5/9) 
+    for temperature in temp_in_celcius:
+        if temperature < 20:
+           cold_temp.append(temperature)
+    # for index, tempis in enumerate(cold_temp,start=1) :
+    #     print(f"{index} - {tempis} Cold")
+    return cold_temp
+        
+f_result = temp_converter([20,30,44,77,30.5,80.9,55,66,77,80,90,9,100])
+print(f_result) 
+print()
+
+#TUPLE UNPACKING IN FUNCTIONS
+#Top performer
+g_results = [('Agaba', 60),('Cliff',45),('Humu',90) ]
+def performance(g_results):  
+  top_student = ''
+  top_mark = 0
+  for name,mark in g_results:
+    if mark > top_mark:
+        top_student = name
+        top_mark = mark
+        
+    else:
+        pass
+  return top_student,top_mark
+s_name, s_mark = performance(g_results)
+print(f"Best performer is {s_name} : {s_mark}")
+
